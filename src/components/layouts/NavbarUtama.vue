31a1a2123b0339ab3b615ccf5ca2f9d8aa95c8be
Adrian nathanael <115018250+AdrianNael@users.noreply.github.com>
2024-01-23 21:57:29 +0700

    Update Login 1.2

--- src/components/layouts/NavbarUtama.vue
+++ src/components/layouts/NavbarUtama.vue
@@ -1,28 +1,18 @@
 <script setup>
-import { onMounted, computed } from 'vue'
-import { useUserStore } from '@/stores/user'
-
+import AuthButton from './AuthButton.vue';
 import Logo from './Logo.vue'
 import NavigationLinks from './NavigationLinks.vue'
-import UserInfo from './UserInfo.vue'
-import AuthButton from './AuthButton.vue'
-
-const userStore = useUserStore()
-const user = computed(() => userStore.getUser)
-const isLoggedIn = computed(() => userStore.isLoggedIn)
-
-onMounted(() => {
-  userStore.fetchUser()
-})
+import UserInfo from './UserInfo.vue';
 </script>
 
 <template>
   <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
-      <div class="container flex flex-wrap items-center justify-between mx-auto my-2">
-          <Logo />
-          <UserInfo v-if="isLoggedIn" :user="user.data" />
-          <AuthButton v-else />
-          <NavigationLinks />
-      </div>
+    <div class="container flex flex-wrap items-center justify-between mx-auto my-2">
+      <Logo />
+      <UserInfo/>
+      <AuthButton />
+      <NavigationLinks />
+
+    </div>
   </nav>
 </template>
